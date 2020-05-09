5a8af33542956f5d9f3b2f0dae6868cf30a5b072
Tae'lur Alexis <taeluralexis@gmail.com>
2020-05-09 19:13:34 -0400

    fixed spelling of São Paulo

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "Sao Paulo, Brazil", "Cairo, Egypt"]
+destinations = ["Paris, France", "Shanghai, China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
 def get_destination_index(destination):
